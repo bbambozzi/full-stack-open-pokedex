1e7d1805eea4cc789018975533c0041f5d89f932
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-02-20 21:04:18 -0300

    Part b finished!

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,11 +1,11 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5050')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('single card can be accessed correctly', () => {
-    cy.visit('http://localhost:5050')
+    cy.visit('http://localhost:5000')
     cy.contains('pikachu').click()
     cy.contains('lightning rod')
     cy.contains('Next')
